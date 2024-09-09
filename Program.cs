f1ca82e88e120080f44f67e86e92f3e4c70c81bf
339713273 <339713273@2260I-L29-026>
2024-09-09 09:34:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Carter Todd
+            Console.WriteLine("I will now count my age");
+            Console.WriteLine(2024 - 2008);
+            Console.WriteLine("I will now count the average of these following numbers");
+            Console.WriteLine((14.0 + 15.0 + 16.0) / 3);  
+       
+            
+
+        }
+    }
+}
