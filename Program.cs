4c01794bcd0ba244865d36186ded4d8555851d9d
339713273 <339713273@2260I-L29-026>
2024-09-10 09:25:28 -0400

    Finished

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@
     {
         static void Main(string[] args)
         {// Carter Todd
-            Console.WriteLine("I will now count my age");
             Console.WriteLine(2024 - 2008);
-            Console.WriteLine("I will now count the average of these following numbers");
-            Console.WriteLine((14.0 + 15.0 + 16.0) / 3);  
-       
-            
+            Console.WriteLine((14.0 + 15.0 + 16.0) / 3);
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.WriteLine(3.14 * (3 * 3));
+            Console.WriteLine("C# knows to add when there are no quotations on the numbers for example it would add when you put (5 + 5) but wouldn't add if you put (Quote5quote + 5)");
+            Console.ReadLine();  
+
+
+
+
+
+
 
         }
     }
